75392f53a264ba3e8f4a75c7098b9a31b305e6a0
cenruyi <rycen@tmindtech.com>
2020-04-18 13:09:48 +0800

    init

---
+++ src/com/andycen/test/map/HashMapTest.java
@@ -0,0 +1,16 @@
+package com.andycen.test.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author cenruyi
+ */
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("a", "AA");
+        map.put("b", "BB");
+    }
+}
